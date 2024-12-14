bedecd28502d2ac85f6a9442050ef8aae9f9203b
Matt Pasker <mattpasker@Matts-MacBook-Air.local>
2024-12-14 13:07:43 -0600

    Day one part 2 solution.

--- 2024/dayOne/main.go
+++ 2024/dayOne/main.go
@@ -11,8 +11,10 @@ import (
 func main() {
 	dayOneInput := fetchInput()
 	part1Solution := part1(dayOneInput)
+	part2Solution := part2(dayOneInput)
 
 	fmt.Println("Day One Part One Solution: ", part1Solution)
+	fmt.Println("Day One Part Two Solution: ", part2Solution)
 }
 
 func fetchInput() string {
@@ -24,9 +26,21 @@ func fetchInput() string {
 }
 
 func part1(input string) int {
-	lines := strings.Split(strings.TrimSpace(input), "\n")
+	lines := formatLines(input)
 	left, right := parseAndSortLists(lines)
 	return calculateDistance(left, right)
+}
+
+func part2(input string) int {
+	lines := formatLines(input)
+	left, right := parseAndSortLists(lines)
+
+	return calculateSimilarityScore(left, right)
+}
+
+func formatLines(input string) []string {
+	formattedString := strings.Split(strings.TrimSpace(input), "\n")
+	return formattedString
 }
 
 func parseAndSortLists(lines []string) ([]int, []int) {
@@ -60,4 +74,19 @@ func calculateDistance(left []int, right []int) int {
 	}
 
 	return totalDistance
+}
+
+func calculateSimilarityScore(left []int, right []int) int {
+	rightMap := make(map[int]int)
+	similarityScore := 0
+	for _, num := range right {
+		rightMap[num]++
+	}
+
+	for _, num := range left {
+		if value, exists := rightMap[num]; exists {
+			similarityScore += num * value
+		}
+	}
+	return similarityScore
 }
